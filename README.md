b7301c0b1e40d78ff403f1cf3a47f9671521e03f
Aliaksandr Sazankou <sazankou.a@gmail.com>
2023-04-23 12:22:56 +0200

    First build

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-# obhfinbricks
+# Finbricks Chat App
+
+Brief description of the project.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [API Documentation](#api-documentation)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+Brief instructions on how to install and set up the project.
+
+## Usage
+
+Detailed instructions on how to use the project, including examples and screenshots if applicable.
+
+## API Documentation
+
+If your project includes an API, provide a link to the API documentation here.
+
+## Contributing
+
+Instructions on how to contribute to the project, including guidelines for pull requests and coding standards.
+
+## License
+
+This project is licensed under the terms of the MIT license. See the [LICENSE](LICENSE) file for details.
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,237 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="chat-window">
+      <h1>Finbricks Chat App</h1>
+      <div class="chat-messages">
+        <div v-for="(message, index) in messages" :key="index" class="message" @keydown.enter="sendMessage">
+          {{ message }}
+        </div>
+      </div>
+      <div class="chat-input">
+        <input type="text" v-model="inputMessage" placeholder="Type a message...">
+        <button class="send-button" @click="sendMessage" :disabled="!inputMessage && !audioBlob">
+          Send
+        </button>
+        <button class="record-button" @click="toggleRecording">
+          <span v-if="!recording">Record</span>
+          <span v-else>Stop</span>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      messages: [],
+      recording: false,
+      mediaRecorder: null,
+      chunks: [],
+      stream: null,
+      apiUrl: 'http://localhost:8000/transcribe',
+      inputMessage: '',
+      audioBlob: null
+    };
+  },
+  methods: {
+    async startRecording() {
+      this.recording = true;
+      this.chunks = [];
+      try {
+        this.stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        this.mediaRecorder = new MediaRecorder(this.stream);
+        this.mediaRecorder.addEventListener('dataavailable', event => {
+          this.chunks.push(event.data);
+        });
+        this.mediaRecorder.addEventListener('stop', async () => {
+          this.recording = false;
+          const blob = new Blob(this.chunks, { type: 'audio/webm' });
+          this.audioBlob = blob;
+        });
+        this.mediaRecorder.start();
+      } catch (error) {
+        console.error(error);
+        this.recording = false;
+      }
+    },
+    stopRecording() {
+      this.mediaRecorder.stop();
+      this.stream.getTracks().forEach(track => track.stop());
+    },
+    toggleRecording() {
+      if (this.recording) {
+        this.stopRecording();
+      } else {
+        this.startRecording();
+      }
+    },
+    async sendMessage() {
+      if (this.audioBlob) {
+        const formData = new FormData();
+        formData.append('audio', this.audioBlob, 'recording.webm');
+        try {
+          const response = await axios.post(this.apiUrl, formData);
+          this.messages.push(response.data);
+          this.audioBlob = null;
+        } catch (error) {
+          console.error(error);
+        }
+
+      }
+      if (this.inputMessage) {
+        // Send the user's message to the chatbot API endpoint
+        try {
+          const response = await axios.post('https://localhost:8080/chatbot', {
+            message: this.inputMessage,
+          });
+
+        // Add the chatbot's response to the chat window
+          this.messages.push(response.data.message);
+        } catch (error) {
+          console.error(error);
+        }
+        this.messages.push(this.inputMessage);
+        this.inputMessage = '';
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i');
+
+html, body {
+  margin: 0; padding: 20px; overflow: hidden;
+  height: 100%;
+  background: linear-gradient(180deg, #2A48E9, #12C3BE);
+  font-family: 'Roboto', sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+h1{
+  text-align: center;  
+  color:#fff;
+}
+/* Chat container */
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+/* Chat messages */
+.chat-messages {
+  flex-grow: 1;
+  padding: 1rem;
+  overflow-y: scroll;
+  background-color: #f7f7f7;
+  height: 70vh;
+  opacity: 0.4;
+}
+
+.message {
+  opacity: 1;
+}
+
+/* Chat input */
+.chat-input {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  background-color: #fff;
+}
+
+/* Chat input text field */
+.chat-input input[type="text"] {
+  flex-grow: 1;
+  padding: 0.5rem;
+  border: none;
+  border-radius: 0.25rem;
+}
+
+/* Chat input send button */
+.chat-input .send-button {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+  color: #fff;
+  background-color: #007bff;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+/* Chat input send button - disabled state */
+.chat-input .send-button:disabled {
+  background-color: #6c757d;
+  cursor: not-allowed;
+}
+
+/* Chat input send button - hover state */
+.chat-input .send-button:hover:not(:disabled) {
+  background-color: #0069d9;
+}
+.chat-messages {
+  flex-grow: 1;
+  padding: 1rem;
+  overflow-y: scroll;
+  background-color: #f7f7f7;
+  flex-direction: column;
+  margin: 0.5rem 0;
+}
+
+/* Chat message sender name */
+.chat-message .sender-name {
+  font-weight: bold;
+  margin-bottom: 0.25rem;
+}
+
+/* Chat message content */
+.chat-message .message-content {
+  background-color: #007bff;
+  color: #fff;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  align-self: flex-start;
+}
+
+/* Chat message from current user */
+.chat-message.current-user .message-content {
+  background-color: #6c757d;
+  align-self: flex-end;
+}
+
+/* Chat input record button */
+.chat-input .record-button {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+  color: #fff;
+  background-color: #007bff;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+/* Chat input record button - disabled state */
+.chat-input .record-button:disabled {
+  background-color: #6c757d;
+  cursor: not-allowed;
+}
+
+/* Chat input record button - hover state */
+.chat-input .record-button:hover:not(:disabled) {
+  background-color: #0069d9;
 }
 </style>
